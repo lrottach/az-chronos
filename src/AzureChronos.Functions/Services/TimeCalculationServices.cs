8183998253bcb5faa48dcc439b8debd798851b65
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-07-11 18:49:26 +0200

    feat: implemented TimeCalculationService
    Build for working with DateTimes and CRON expressions

--- src/AzureChronos.Functions/Services/TimeCalculationServices.cs
+++ src/AzureChronos.Functions/Services/TimeCalculationServices.cs
@@ -1,6 +1,39 @@
+using System;
+using Cronos;
+
 namespace AzureChronos.Functions.Services;
 
 public class TimeCalculationServices
 {
+    /// <summary>
+    /// Function to get the next occurrence of a cron expression
+    /// </summary>
+    /// <param name="cronExpression"></param>
+    /// <returns></returns>
+    public DateTime? GetNextOccurrence(string cronExpression)
+    {
+        var cron = CronExpression.Parse(cronExpression);
+        var nextOccurrence = cron.GetNextOccurrence(DateTime.UtcNow, TimeZoneInfo.Utc);
+        
+        return nextOccurrence;
+    }
     
+    /// <summary>
+    /// Function to validate if a DateTime is within the specified number of minutes from now
+    /// </summary>
+    /// <param name="dateTime">DateTime to validate. Generated from a CRON expression for example</param>
+    /// <param name="validationMinutes"></param>
+    /// <returns></returns>
+    public bool IsWithinNext30Minutes(DateTime? dateTime, int validationMinutes)
+    {
+        if (dateTime == null)
+        {
+            return false;
+        }
+        
+        var now = DateTime.UtcNow;
+        var thirtyMinutesFromNow = now.AddMinutes(validationMinutes);
+        
+        return dateTime >= now && dateTime <= thirtyMinutesFromNow;
+    }
 }
