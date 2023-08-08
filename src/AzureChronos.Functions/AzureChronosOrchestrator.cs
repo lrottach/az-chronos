276eb6700c8c6edf6b9f61745ce87bd7e25a5d41
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-08-08 22:53:11 +0200

    feat: add Subscription ID to VM listing function
    Added an ability to retrieve Azure Subscription ID from environment variables and pass it to the ListAzureVirtualMachines function. This change reflects the updated requirements for Azure's Resource Manager APIs, which now require the Subscription ID for operations on resources.

--- src/AzureChronos.Functions/AzureChronosOrchestrator.cs
+++ src/AzureChronos.Functions/AzureChronosOrchestrator.cs
@@ -3,6 +3,7 @@ using AzureChronos.Functions.Interfaces;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace AzureChronos.Functions;
 
@@ -21,6 +22,8 @@ public class AzureChronosOrchestrator
         ILogger log)
     {
         log.LogInformation("Starting AzureChronosOrchestrator");
-        // var vmList = await _azureComputeService.ListAzureVirtualMachines("");
+        var subId = Environment.GetEnvironmentVariable("AZ_SUBSCRIPTION_ID");
+        Console.WriteLine($"Subscription ID: {subId}");
+        var vmList = await _azureComputeService.ListAzureVirtualMachines(subId);
     }
 }
