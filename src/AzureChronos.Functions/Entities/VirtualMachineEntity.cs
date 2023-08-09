0814e09967ccbfc1adcefa635ed5cf946a14f82e
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-08-09 17:03:32 +0200

    feat: add IVirtualMachineEntity interface and implementation
    Introduced a new interface IVirtualMachineEntity and its corresponding implementation in VirtualMachineEntity class. These changes were necessary to define an entity in the application which represents a virtual machine. The InitializeEntityAsync method was added to set up the entity with provided details.

---
+++ src/AzureChronos.Functions/Entities/VirtualMachineEntity.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using AzureChronos.Functions.Interfaces;
+using AzureChronos.Functions.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureChronos.Functions.Entities;
+
+[JsonObject(MemberSerialization.OptIn)]
+public class VirtualMachineEntity : IVirtualMachineEntity
+{
+    private readonly ILogger _log;
+    private readonly IAzureComputeService _azureComputeService;
+    
+    [JsonProperty("subscriptionId")]
+    public string SubscriptionId { get; set; }
+    
+    [JsonProperty("resourceGroupName")]
+    public string ResourceGroupName { get; set; }
+    
+    [JsonProperty("virtualMachineName")]
+    public string VirtualMachineName { get; set; }
+    
+    [JsonProperty("scheduled")]
+    public bool Scheduled { get; set; }
+    
+    public VirtualMachineEntity(ILogger log, IAzureComputeService azureComputeService)
+    {
+        _log = log;
+        _azureComputeService = azureComputeService;
+    }
+    
+    public Task InitializeEntityAsync(EntityInitializePayload payload)
+    {
+        SubscriptionId = payload.SubscriptionId;
+        ResourceGroupName = payload.ResourceGroupName;
+        VirtualMachineName = payload.VirtualMachineName;
+        Scheduled = false;
+        
+        return Task.CompletedTask;
+    }
+    
+    [FunctionName(nameof(VirtualMachineEntity))]
+    public static Task Run([EntityTrigger] IDurableEntityContext ctx, ILogger log)
+        => ctx.DispatchAsync<VirtualMachineEntity>(log);
+
+}
---
+++ src/AzureChronos.Functions/Interfaces/IVirtualMachineEntity.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+using AzureChronos.Functions.Models;
+
+namespace AzureChronos.Functions.Interfaces;
+
+public interface IVirtualMachineEntity
+{
+    public Task InitializeEntityAsync(EntityInitializePayload payload);
+}
