b83167db4867a58cb5af4e672e93b5e32e42df0c
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-08-09 17:04:17 +0200

    feat: implemented Azure Compute Service to Timer Trigger
    Added Azure Compute Service to Azure Chronos Timer Trigger. This change allows for more granular control and querying of Azure virtual machines. The code now enumerates all VMs in the subscription, skips those for which an event has already been scheduled, and initializes an entity for new ones. It's aimed to enhance the management and scalability of Chronos service by allowing interactions with VMs on a subscription basis.

--- src/AzureChronos.Functions/AzureChronosTimerTrigger.cs
+++ src/AzureChronos.Functions/AzureChronosTimerTrigger.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Threading.Tasks;
+using AzureChronos.Functions.Common;
+using AzureChronos.Functions.Interfaces;
+using AzureChronos.Functions.Entities;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
@@ -8,13 +11,52 @@ namespace AzureChronos.Functions;
 
 public class AzureChronosTimerTrigger
 {
+    private readonly IAzureComputeService _azureComputeService;
+
+    public AzureChronosTimerTrigger(IAzureComputeService azureComputeService)
+    {
+        _azureComputeService = azureComputeService;
+    }
+    
     [FunctionName("AzureChronosTimerTrigger")]
     public async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer,
-        [DurableClient] IDurableOrchestrationClient starter,
+        [DurableClient] IDurableClient client,
         ILogger log)
     {
-        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        string instanceId = await starter.StartNewAsync("AzureChronosOrchestrator", null); 
-        log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+        // Gather required environment variables
+        var subscriptionId = Environment.GetEnvironmentVariable("AZ_SUBSCRIPTION_ID");
+        
+        log.LogInformation($"[TimerTrigger] C# Timer trigger function executed at: {DateTime.Now}");
+        
+        // Query a list of all virtual machines in the subscription
+        var virtualMachines = await _azureComputeService.ListAzureVirtualMachines(subscriptionId);
+        
+        log.LogInformation("[TimerTrigger] Found {vmCount} virtual machines in subscription {subscriptionId}", virtualMachines.Count, subscriptionId);
+
+        foreach (var vm in virtualMachines)
+        {
+            // Validate if an entity already exists and an event was already scheduled
+            var entityId = new EntityId(nameof(VirtualMachineEntity), vm.Id.ToString().Replace("/", ""));
+            var entityState = await client.ReadEntityStateAsync<VirtualMachineEntity>(entityId);
+
+            // If the entity already exists and an event was already scheduled, skip
+            if (entityState.EntityExists && entityState.EntityState.Scheduled)
+            {
+                log.LogWarning(
+                    "[TimerTrigger] Entity for Azure Virtual Machine '{vmId}' already exists and Azure Chronos is already scheduled. Skipping...",
+                    vm.Id);
+                continue;
+            }
+
+            // Initialize the entity
+            log.LogInformation("[TimerTrigger] Initializing entity for Azure Virtual Machine '{vmId}'", vm.Id);
+            await client.SignalEntityAsync<IVirtualMachineEntity>(entityId, proxy => proxy.InitializeEntityAsync(
+                new Models.EntityInitializePayload
+                {
+                    SubscriptionId = subscriptionId,
+                    ResourceGroupName = StringHandler.ExtractResourceGroupName(vm.Id.ToString()),
+                    VirtualMachineName = vm.Data.Name
+                }));
+        }
     }
 }
