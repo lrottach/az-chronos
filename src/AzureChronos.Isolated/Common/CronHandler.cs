a105d599342fa307063a969e755ea6bd1c8704ed
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-08-22 16:38:49 +0200

    feat: added CronHandler

---
+++ src/AzureChronos.Isolated/Common/CronHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NCrontab;
+
+namespace AzureChronos.Isolated;
+
+public static class CronHandler
+{
+    /// <summary>
+    /// Calculates the next occurrence of a cron expression in UTC time zone.
+    /// </summary>
+    /// <param name="cronExpression">The cron expression to evaluate.</param>
+    /// <returns>The next occurrence of the cron expression in UTC time zone, or null if the expression is invalid.</returns>
+    public static DateTime? GetNextOccurrence(string cronExpression)
+    {
+        try
+        {
+            var expression = CrontabSchedule.Parse(cronExpression);
+            var nextOccurrence = expression.GetNextOccurrence(DateTime.UtcNow);
+            return nextOccurrence;
+        }
+        catch (Exception)
+        {
+            return null; 
+        }
+    }
+    
+    /// <summary>
+    /// Determines whether the specified date and time is within the next specified number of minutes from the current UTC date and time.
+    /// </summary>
+    /// <param name="dateTime">The date and time to compare.</param>
+    /// <param name="validationMinutes">The number of minutes to validate.</param>
+    /// <returns>true if the specified date and time is within the next specified number of minutes from the current UTC date and time; otherwise, false.</returns>
+    public static bool IsDateTimeInRange(DateTime? dateTime, int validationMinutes)
+    {
+        if (dateTime == null)
+        {
+            return false;
+        }
+        
+        var now = DateTime.UtcNow;
+        var thirtyMinutesFromNow = now.AddMinutes(validationMinutes);
+        
+        return dateTime >= now && dateTime <= thirtyMinutesFromNow;
+    }
+}
